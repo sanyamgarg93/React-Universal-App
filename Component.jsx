25ee78f6b611d1cabbe3b9f0918d2fe1176aa91f
sanyamgarg93 <sanyamgarg93@yahoo.in>
2018-02-15 14:23:01 +0530

    Added client build

--- Component.jsx
+++ Component.jsx
@@ -11,11 +11,22 @@ module.exports = createReactClass({
 		//return React.createElement('h1', null, 'Hello World!');
 
 		return (
-			<div>
-				<h1>Hello World!</h1>
-				<p>This is a paragraph</p>
-				<button onClick = {this._handleClick}>Click Me</button>
-			</div>
-		)
+
+			<html>
+                <head>
+                    <title>Universal App with React</title>
+                    <link rel='stylesheet' href='/style.css' />
+                </head>
+                <body>
+                    <div>
+                        <h1>Hello World!</h1>
+                        <p>This is a paragraph.</p>
+                        <button onClick={this._handleClick}>Click Me</button>
+                    </div>
+                    <script src='/bundle.js' />
+                </body>
+            </html>
+				
+		);
 	}
 });
--- server.js
+++ server.js
@@ -10,6 +10,12 @@ var React = require('react');
 var ReactDOMServer = require('react-dom/server');
 var Component = require('./Component.jsx');
 
+
+
+// Express middleware
+app.use(express.static('public'));
+
+
 // Route for the index page
 app.get('/', function(request, response){
 	//var html = '<h1>Hello World!</h1>';
@@ -21,6 +27,8 @@ app.get('/', function(request, response){
 	response.send(html);
 });
 
+
+// For Production: // var PORT = process.env.PORT || 3000;
 var PORT = 5000;
 
 app.listen(PORT, function(){
